382c985b399363e47b73973f70358c30913425eb
Ragunath Jawahar <ragunath@redgreen.io>
2022-03-26 21:59:03 +0530

    refactor!!: override

--- TMessagesProj/src/main/java/org/telegram/tgnet/ConnectionsManagerTestable.java
+++ TMessagesProj/src/main/java/org/telegram/tgnet/ConnectionsManagerTestable.java
@@ -4,4 +4,11 @@ public class ConnectionsManagerTestable extends ConnectionsManager {
     public ConnectionsManagerTestable(int instance) {
         super(instance);
     }
+
+    public static int nativeGetConnectionState = ConnectionStateConnected;
+
+    @Override
+    protected int getNativeGetConnectionState() {
+        return nativeGetConnectionState;
+    }
 }
