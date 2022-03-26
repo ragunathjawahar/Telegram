ebdd2531cb0cbbdaf193b5989fd862e6787c4a35
Ragunath Jawahar <ragunath@redgreen.io>
2022-03-26 16:53:43 +0530

    test: verify approvals setup

--- TMessagesProj/src/test/java/LitmusJUnit4Test.java
+++ TMessagesProj/src/test/java/LitmusJUnit4Test.java
@@ -1,3 +1,4 @@
+import org.approvaltests.Approvals;
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -7,5 +8,10 @@ public class LitmusJUnit4Test {
     public void junit_4_vintage_engine_is_setup() {
         assertThat(true)
                 .isTrue();
+    }
+
+    @Test
+    public void approvals_is_setup() {
+        Approvals.verify("Hello, JUnit 4!");
     }
 }
--- TMessagesProj/src/test/java/LitmusJUnit5Test.java
+++ TMessagesProj/src/test/java/LitmusJUnit5Test.java
@@ -1,3 +1,4 @@
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -7,5 +8,10 @@ public class LitmusJUnit5Test {
     void junit_5_is_setup() {
         assertThat(true)
                 .isTrue();
+    }
+
+    @Test
+    void approvals_is_setup() {
+        Approvals.verify("Hello, JUnit 5!");
     }
 }
