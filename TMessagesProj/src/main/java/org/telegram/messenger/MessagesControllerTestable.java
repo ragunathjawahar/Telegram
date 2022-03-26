4c9d0d0323e1aa05c7ad54c07c285dc72a5db683
Ragunath Jawahar <ragunath@redgreen.io>
2022-03-26 21:47:26 +0530

    style: move members closer to usage

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesControllerTestable.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesControllerTestable.java
@@ -4,13 +4,13 @@ import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 public class MessagesControllerTestable extends MessagesController {
-    public static final String DC_DOMAIN_NAME_TAPV_3_STEL_COM = "tapv3.stel.com";
-    public static final String DC_DOMAIN_NAME_APV_3_STEL_COM = "apv3.stel.com";
-    public static String dcDomainName2 = DC_DOMAIN_NAME_TAPV_3_STEL_COM;
-
     public MessagesControllerTestable(int num) {
         super(num);
     }
+
+    public static final String DC_DOMAIN_NAME_TAPV_3_STEL_COM = "tapv3.stel.com";
+    public static final String DC_DOMAIN_NAME_APV_3_STEL_COM = "apv3.stel.com";
+    public static String dcDomainName2 = DC_DOMAIN_NAME_TAPV_3_STEL_COM;
 
     @NotNull
     @Override
