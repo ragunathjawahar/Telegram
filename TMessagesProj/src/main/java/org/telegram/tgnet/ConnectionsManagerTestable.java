6209647bf9f26590679e484fa2ba64700b056ccc
Ragunath Jawahar <ragunath@redgreen.io>
2022-03-26 21:50:52 +0530

    feat: subclass `ConnectionsManager` for testability

---
+++ TMessagesProj/src/main/java/org/telegram/tgnet/ConnectionsManagerTestable.java
@@ -0,0 +1,7 @@
+package org.telegram.tgnet;
+
+public class ConnectionsManagerTestable extends ConnectionsManager {
+    public ConnectionsManagerTestable(int instance) {
+        super(instance);
+    }
+}
